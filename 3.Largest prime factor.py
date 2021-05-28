13ab98760eaf5565f79e065126954bb3ba632a6a
venkatesh-vk <61019595+venkatesh-vk@users.noreply.github.com>
2021-05-28 22:59:47 +0530

    Update 3.Largest prime factor.py

--- 3.Largest prime factor.py
+++ 3.Largest prime factor.py
@@ -3,12 +3,13 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 
 What is the largest prime factor of the number 600851475143 ?
 '''
-n=600851475143
+import sympy as s
+import math as m
+n=m.floor(m.sqrt(600851475143))
+b=600851475143
 a=[]
-b=[]
-for i in range(2,n+1):
-    if i not in a:
-        b.append(i)
-        for j in range(i**2,n+1,i):
-            a.append(j)
-print(max(b))
+for i in range(2,n,1):
+    if(s.isprime(i)):
+        if(b%i==0):
+            a.append(i)
+print(max(a))
